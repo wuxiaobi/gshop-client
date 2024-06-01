44ba15d53bf433d5ae452392195aed4307080405
wuxiaobi <422249663@qq.com>
2024-06-01 15:11:21 +0800

    p11、尚硅谷_Vue项目_HeaderTop组件

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -450,11 +450,11 @@ export default {
                         &:last-child
                           margin-right: 0
                         &.on
-                          bg-image('./images/stars/star48_on')
+                          bg-image('/src/pages/MSite/images/stars/star48_on')
                         &.half
-                          bg-image('./images/stars/star48_half')
+                          bg-image('/src/pages/MSite/images/stars/star48_half')
                         &.off
-                          bg-image('./images/stars/star48_off')
+                          bg-image('/src/pages/MSite/images/stars/star48_off')
                     &.star-36
                       .star-item
                         width 15px
@@ -464,11 +464,11 @@ export default {
                         &:last-child
                           margin-right 0
                         &.on
-                          bg-image('./images/stars/star36_on')
+                          bg-image('/src/pages/MSite/images/stars/star36_on')
                         &.half
-                          bg-image('./images/stars/star36_half')
+                          bg-image('/src/pages/MSite/images/stars/star36_half')
                         &.off
-                          bg-image('./images/stars/star36_off')
+                          bg-image('/src/pages/MSite/images/stars/star36_off')
                     &.star-24
                       .star-item
                         width 10px
@@ -478,11 +478,11 @@ export default {
                         &:last-child
                           margin-right 0
                         &.on
-                          bg-image('./images/stars/star24_on')
+                          bg-image('/src/pages/MSite/images/stars/star24_on')
                         &.half
-                          bg-image('./images/stars/star24_half')
+                          bg-image('/src/pages/MSite/images/stars/star24_half')
                         &.off
-                          bg-image('./images/stars/star24_off')
+                          bg-image('/src/pages/MSite/images/stars/star24_off')
                   .rating_section
                     float left
                     font-size 10px
